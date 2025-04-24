a5dff9c719316db7b4845ee5c448806e028e1e13
MoloZzz <mega_oleksii@ukr.net>
2025-04-24 11:03:04 +0300

    feat: get portfolio by profileId, id

--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -1,21 +1,34 @@
-import { Body, Controller, Delete, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PortfolioService } from './portfolio.service';
 import { CreatePortfolioDto, UUIDParamDto } from 'src/common/dto';
 import { Request } from 'express';
 import { IProfile } from 'src/common/interface/profile.interface';
 import { JwtAuthGuard } from 'src/common/guard/jwt-auth.guard';
+import { PortfolioModel } from 'src/common/sequelize/models/portfolio.model';
 
 @ApiTags('Portfolio API')
 @Controller('portfolio')
 export class PortfolioController {
     constructor(private readonly portfolioService: PortfolioService) {}
 
+    @Get('profile/:id')
+    @ApiOperation({ summary: 'Get all portfolios by profile id' })
+    async getPortfoliosByProfileId(@Param() params: UUIDParamDto) {
+        return this.portfolioService.getPortfoliosByProfileId(params.id);
+    }
+
+    @Get(':id')
+    @ApiOperation({ summary: 'Get portfolio by id' })
+    async getPortfolioById(@Param() params: UUIDParamDto) : Promise<PortfolioModel> {
+        return this.portfolioService.getPortfolioById(params.id);
+    }
+
     @Post()
     @UseGuards(JwtAuthGuard)
     @ApiOperation({ summary: 'Create a new portfolio' })
     @ApiBearerAuth()
-    async create(@Body() data: CreatePortfolioDto, @Req() req: Request) {
+    async create(@Body() data: CreatePortfolioDto, @Req() req: Request): Promise<PortfolioModel> {
         const profile: IProfile = req.user as IProfile;
         return this.portfolioService.create(profile.id, data);
     }
@@ -24,7 +37,7 @@ export class PortfolioController {
     @UseGuards(JwtAuthGuard)
     @ApiOperation({ summary: 'Delete your portfolio' })
     @ApiBearerAuth()
-    async delete(@Param() params: UUIDParamDto, @Req() req: Request) {
+    async delete(@Param() params: UUIDParamDto, @Req() req: Request): Promise<void> {
         const profile: IProfile = req.user as IProfile;
         return this.portfolioService.delete(profile.id, params.id);
     }
--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -1,7 +1,11 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreatePortfolioDto } from 'src/common/dto';
+import { CommentModel } from 'src/common/sequelize/models/comment.model';
+import { FileModel } from 'src/common/sequelize/models/file.model';
+import { ImageModel } from 'src/common/sequelize/models/image.model';
 import { PortfolioModel } from 'src/common/sequelize/models/portfolio.model';
+import { ProfileModel } from 'src/common/sequelize/models/profile.model';
 
 @Injectable()
 export class PortfolioService {
@@ -10,14 +14,58 @@ export class PortfolioService {
         private readonly portfolioModel: typeof PortfolioModel,
     ) {}
 
-    async create(id: string, data: CreatePortfolioDto) {
+    async getPortfoliosByProfileId(profileId: string): Promise<PortfolioModel[]> {
+        return await this.portfolioModel.findAll({
+            where: { profileId },
+            attributes: ['id', 'name', 'description'],
+            include: [
+                {
+                    model: ProfileModel, 
+                    attributes: ['id', 'username'],
+                },
+                {
+                    model: ImageModel, 
+                    attributes: ['id', 'name', 'description'],
+                    include: [
+                         { model: FileModel, attributes: ['id', 'path', 'originalname', 'mimetype', 'size'] }, 
+                         { model: CommentModel, attributes: ['id','content', 'profileId']}, 
+                    ],
+                },
+            ],
+             order: [['createdAt', 'DESC']],
+        });
+    }
+
+    async getPortfolioById(id: string) : Promise<PortfolioModel>{
+        return await this.portfolioModel.findOne({
+            where: { id },
+            attributes: ['id', 'name', 'description'],
+            include: [
+                {
+                    model: ProfileModel, 
+                    attributes: ['id', 'username'],
+                },
+                {
+                    model: ImageModel, 
+                    attributes: ['id', 'name', 'description'],
+                    include: [
+                         { model: FileModel, attributes: ['id', 'path', 'originalname', 'mimetype', 'size'] }, 
+                         { model: CommentModel, attributes: ['id','content', 'profileId']}, 
+                    ],
+                },
+            ],
+             order: [['createdAt', 'DESC']],
+        });
+    }
+
+    async create(id: string, data: CreatePortfolioDto) : Promise<PortfolioModel> {
         return await this.portfolioModel.create({
             ...data,
             profileId: id,
         });
     }
 
-    async delete(profileId: string, portfolioId: string) {
+    async delete(profileId: string, portfolioId: string): Promise<void> {
         const portfolio = await this.portfolioModel.findOne({
             where: {
                 id: portfolioId,
@@ -32,7 +80,6 @@ export class PortfolioService {
         }
 
         await portfolio.destroy();
-        return { message: `Portfolio ${portfolioId} deleted successfully` };
     }
 
     async isAuthor(profileId: string, portfolioId: string): Promise<boolean> {
